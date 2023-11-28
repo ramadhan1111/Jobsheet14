7e93d1035a92fec640189ffe9cd963e3a08b5697
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-11-28 14:30:16 +0700

    Fibonacci25.java

--- daspro-jobsheet14/Fibonacci25.java
+++ daspro-jobsheet14/Fibonacci25.java
@@ -1,21 +1,29 @@
-public class PopulasiMarmutRekursif {
-
-    public static int populasi(int bulan) {
-        if (bulan == 1) {
-            return 1;
-        } else {
-            return populasi(bulan - 2) * 2;
-        }
-    }
+import java.util.Scanner;
+public class Fibonacci25 {
 
     public static void main(String[] args) {
-        int bulan = 12;
-        int populasi = populasi(bulan);
+        System.out.println("==========================================================");
+        System.out.println("|  Bulan  | Produktif | Belum Produktif | Total Pasangan |");
+        System.out.println("==========================================================");
 
-        System.out.println("Bulan | Populasi");
-        System.out.println("------- | --------");
-        for (int i = 1; i <= bulan; i++) {
-            System.out.println(i + " | " + populasi(i));
+        for (int i = 1; i <= 12; i++) {
+            int jumlahProduktif = hitungJumlahPasanganMarmutProduktif(i);
+            int jumlahBelumProduktif = hitungJumlahPasanganMarmutBelumProduktif(i);
+            int jumlahTotal = jumlahProduktif + jumlahBelumProduktif;
+
+            System.out.printf("|%-8d| %-11d| %-16d| %-14d |\n", i, jumlahProduktif, jumlahBelumProduktif, jumlahTotal);
         }
+
+        System.out.println("==========================================================");
+    }
+
+    public static int hitungJumlahPasanganMarmutProduktif(int bulan) {
+        int[] produktif = {0, 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+        return produktif[bulan - 1];
+    }
+
+    public static int hitungJumlahPasanganMarmutBelumProduktif(int bulan) {
+        int[] belumProduktif = {1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+        return belumProduktif[bulan - 1];
     }
 }
