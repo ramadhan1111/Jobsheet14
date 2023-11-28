ae456604e5ebe3b482075ef5dd76917c2ec90709
ramadhan1111 <144088505+ramadhan1111@users.noreply.github.com>
2023-11-28 14:46:05 +0700

    CekPrimaRekursif25.java

--- daspro-jobsheet14/CekPrimaRekursif25.java
+++ daspro-jobsheet14/CekPrimaRekursif25.java
@@ -1,20 +1,29 @@
+import java.util.Scanner;
+
 public class CekPrimaRekursif25 {
 
-    public static boolean prima(int n) {
+    public static boolean cekPrima(int n) {
         if (n <= 1) {
             return false;
-        }
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                return false;
+        } else {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
     }
 
     public static void main(String[] args) {
-        int n = Integer.parseInt(System.console().readLine("Masukkan n: "));
-        boolean hasil = prima(n);
-        System.out.println("n = " + n + " adalah bilangan prima: " + hasil);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        if (cekPrima(n)) {
+            System.out.println(n + " adalah bilangan prima");
+        } else {
+            System.out.println(n + " bukan bilangan prima");
+        }
     }
 }
